1538f41571a8221cf51b9180f97a4869e87accb7
Andrew Eberle <andrew@shareablemetrics.com>
2018-04-07 20:51:45 -0400

    adding root route and favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'schedule/index'
 
+  root 'schedule#index'
+
   resources :schedule
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
