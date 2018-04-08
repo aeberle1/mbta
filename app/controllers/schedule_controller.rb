db24398048635938513d19ff4a827ac6bfafb387
Andrew Eberle <andrew@shareablemetrics.com>
2018-04-08 14:40:29 -0400

    added some tweaks and exception handling

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,4 +1,3 @@
-# require 'rubygems'
 require 'open-uri'
 require 'csv'
 
@@ -7,17 +6,16 @@ class ScheduleController < ApplicationController
   def index
     @schedule_items = []
 
-    CSV.new(open("http://developer.mbta.com/lib/gtrtfs/Departures.csv"), :headers => :first_row).each do |line|
-      puts line
-      @schedule_items << line
-      # @schedule_items << Time.at(line[0].to_i)
+    begin
+      mbta_csv = CSV.new(open("http://developer.mbta.com/lib/gtrtfs/Departures.csv"), :headers => :first_row)
+      mbta_csv.each do |line|
+        @schedule_items << line
+      end
+      @north_station = @schedule_items.select {|station| station["Origin"] == 'North Station' }
+      @south_station = @schedule_items.select {|station| station["Origin"] =='South Station' }
+    rescue =>e
+      @error= e.message
     end
-    @north_station = @schedule_items.select {|station| station["Origin"] == 'North Station' }
-    @south_station = @schedule_items.select {|station| station["Origin"] =='South Station' }
+
   end
 end
-
-# CSV.foreach(myfile.path) do |row|
-#   rowarray << row
-#   @rowarraydisp = rowarray
-# end
